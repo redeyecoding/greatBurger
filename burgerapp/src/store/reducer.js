01eb995c0bd73f2f24a3425bbf8abd0c2cafcf10
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-02 07:23:42 -0500

    initial commit

---
+++ burgerapp/src/store/reducer.js
@@ -0,0 +1,29 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    ingredients: {
+        bacon: 0,
+        salad: 0,
+        cheese: 0,
+        meat: 0
+    },
+    totalPrice: 4,
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [ action.ingredientName ]: state.ingredients[ action.ingredientName ] + 1
+                }
+            }
+        case actionTypes.REMOVE_INGREDIENT:
+            return
+    }
+};
+
+
+export default reducer;
