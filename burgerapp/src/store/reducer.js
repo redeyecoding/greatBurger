2ffb2ca703b3ee0e200eb3d54b1f3810ec778ad0
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-02 10:26:45 -0500

    added default pricing for ingredients

--- burgerapp/src/store/reducer.js
+++ burgerapp/src/store/reducer.js
@@ -1,5 +1,12 @@
 import * as actionTypes from './actions';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+
 const initialState = {
     ingredients: {
         salad: 0,
@@ -7,11 +14,17 @@ const initialState = {
         cheese: 0,
         meat: 0
     },
-    totalPrice: 4,
+    totalPrice: 0,
 };
 
+const updatePurchaseState = () => {
+    console.log(initialState.ingredients)
+};
+
+
 const reducer = (state = initialState, action) => {
-    const { type, ingredientName } = action
+    const { type, ingredientName, ingredientPrice } = action;
+    updatePurchaseState()
     switch(type) {
         case actionTypes.ADD_INGREDIENT:
             return {
@@ -19,7 +32,9 @@ const reducer = (state = initialState, action) => {
                 ingredients: {
                     ...state.ingredients,
                     [ ingredientName ]: state.ingredients[ ingredientName ] + 1
-                }
+                },
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[ ingredientName ],
+                
             }
         case actionTypes.REMOVE_INGREDIENT:
             return {
@@ -27,7 +42,12 @@ const reducer = (state = initialState, action) => {
                 ingredients: {
                     ...state.ingredients,
                     [ ingredientName ]: state.ingredients[ ingredientName ] - 1
-                }
+                },
+                totalPrice: state.totalPrice - INGREDIENT_PRICES[ ingredientName ],
+            }
+        case actionTypes.ORDER_BURGER:
+            return {
+
             }
         default:
             return state;
